acf7e0a81f8996b453550a69d1e4b7ceea29b5db
Darren Rodgers <darren_w_rodgers@live.ie>
2026-01-09 16:05:01 +0000

    Refine error messages

--- src/main/java/com/rodgers/fines/web/user/SignupController.java
+++ src/main/java/com/rodgers/fines/web/user/SignupController.java
@@ -2,6 +2,7 @@ package com.rodgers.fines.web.user;
 
 import com.rodgers.fines.web.webclient.HttpHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,11 +28,25 @@ public class SignupController {
     public String signupSubmit(@ModelAttribute SignUpVo signup, Model model) {
         model.addAttribute("signup", signup);
         signup.setPassword(ENCODER.encode(signup.getPassword()));
-        log.info("Got sign up request : {}", signup);
+        log.info("Got sign up request for username : {}", signup.getUserName());
         HttpResponse<String> response = HttpHelper.createUser(signup);
         log.info("Response from creation service | {}:{}",response.statusCode(),response.body());
-        signup.setResult(response.body());
+        setResult(signup, response);
         return "signup";
     }
 
+    private static void setResult(SignUpVo signup, HttpResponse<String> response) {
+        if(response.statusCode() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            log.warn("Internal error requesting user creation");
+            signup.setResult("Sorry something has gone wrong, please wait and try again later");
+        } else if(response.statusCode() == HttpStatus.BAD_REQUEST.value()) {
+            log.warn("Bad User Request: {}", response.body());
+            signup.setResult(response.body().split(":")[1].replace("}",""));
+        } else if(response.statusCode() == HttpStatus.OK.value()) {
+            log.info("Successful User creation");
+            signup.setResult("User created successfully, please proceed to Login");
+        } else {
+            signup.setResult(response.body());
+        }
+    }
 }
